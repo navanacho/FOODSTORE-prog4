65f1a252d16e1789bb52931209bfcd71fb0c2e27
navanacho <navarria333@gmail.com>
2026-04-24 17:34:17 -0300

    Actualizacion general

--- Frontend/frontend/src/pages/categorias/useCategoriaApi.ts
+++ Frontend/frontend/src/pages/categorias/useCategoriaApi.ts
@@ -3,7 +3,7 @@ import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 import { api } from '../../api/client'
 import type { Categoria } from '../../types'
 
-// ✅ Tipo auxiliar: ID requerido + resto opcional para actualizaciones
+// Tipo auxiliar: ID requerido + resto opcional para actualizaciones
 type UpdateCategoriaInput = { id: number } & Partial<Omit<Categoria, 'id' | 'created_at'>>
 
 export const useCategorias = () => {
